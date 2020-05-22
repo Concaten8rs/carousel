bb1661470f5d878b1191929a4ededa78dec50ecd
serviolee <serviolee@gmail.com>
2020-05-22 15:58:12 -0700

    added foreign keys

--- postgresql-schema.sql
+++ postgresql-schema.sql
@@ -1,18 +1,36 @@
-DROP DATABASE IF EXISTS carousel;
-CREATE DATABASE carousel;
+-- DROP DATABASE IF EXISTS carousel;
+-- CREATE DATABASE carousel;
 
-\c carousel;
+-- \c carousel;
 
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   product_name TEXT NOT NULL,
   product_description TEXT NOT NULL,
   cost INT NOT NULL
-)
+);
 
 CREATE TABLE photos (
   id SERIAL PRIMARY KEY,
-  product_id INT REFERENCES products(id),
-  photo_title TEXT NOT NULL,
+  product_id INT NOT NULL,
+  photo_description TEXT NOT NULL,
+  photo_url TEXT,
   posting_date DATE NOT NULL
-)
+);
+
+CREATE TABLE products_photos (
+  products_id INT NOT NULL,
+  photos_id INT NOT NULL
+);
+
+-- add back foreign keys
+ALTER TABLE photos
+  FOREIGN KEY(product_id) REFERENCES products(id)
+
+ALTER TABLE products_photos
+  FOREIGN KEY(products_id) REFERENCES products(id)
+
+ALTER TABLE products_photos
+  FOREIGN KEY(photos_id) REFERENCES photos(id)
+
+
