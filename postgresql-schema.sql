2384c3bdc1b77bf26a6cfb7c60a6132aab9bcb06
serviolee <serviolee@gmail.com>
2020-05-27 00:30:31 -0700

    created seeding script

---
+++ postgresql-schema.sql
@@ -0,0 +1,28 @@
+-- DROP DATABASE IF EXISTS carousel;
+-- CREATE DATABASE carousel;
+
+-- \c carousel;
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  product_name TEXT NOT NULL,
+  product_description TEXT NOT NULL,
+  cost INT NOT NULL
+);
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  product_id INT NOT NULL,
+  photo_description TEXT NOT NULL,
+  photo_order SMALLINT NOT NULL,
+  photo_url TEXT NOT NULL,
+  posting_date DATE
+);
+
+-- add back foreign keys
+-- ALTER TABLE photos
+--   ADD FOREIGN KEY(product_id) REFERENCES products(id);
+
+COPY products (id, product_name, product_description, cost) FROM '/Users/serviolee/Documents/nextchapter/hrsf127-sdc/carousel/data-generator/productsData.csv' DELIMITER ',' CSV;
+
+COPY photos (id, product_id, photo_description, photo_order, photo_url, posting_date) FROM '/Users/serviolee/Documents/nextchapter/hrsf127-sdc/carousel/data-generator/photosData.csv' DELIMITER ',' CSV;
