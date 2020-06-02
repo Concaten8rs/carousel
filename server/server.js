e4575388d858bb5e8bd863da3e811c4b13495087
serviolee <serviolee@gmail.com>
2020-06-02 10:29:52 -0700

    reconfigured server files

---
+++ server/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('./controllers.js')
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '../client/dist'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.use("/", controllers);
+
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(`error listening to port ${error}`);
+  } else {
+  console.log(`listening on port ${port}`);
+  }
+});
